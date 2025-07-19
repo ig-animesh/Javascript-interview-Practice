ccc440c019e5c91f5537a5c3ed9c8c1581fe641a
Animesh <animesh.h2004@gmail.com>
2025-07-20 01:30:53 +0530

    completed accordian

--- Accordion/main.js
+++ Accordion/main.js
@@ -2,22 +2,22 @@ const data = [
     {
         id: '1',
         question: "What are accordion components?",
-        answer: "Accordion components are user interface elements"
+        answer: "The accordion component is a UI element that allows users to show and hide sections of related content on a page."
     },
     {
         id: '2',
         question: "What are accordion components?",
-        answer: "Accordion components are user interface elements"
+        answer: "The accordion component is a UI element that allows users to show and hide sections of related content on a page."
     },
     {
         id: '3',
         question: "What are accordion components?",
-        answer: "Accordion components are user interface elements"
+        answer: "The accordion component is a UI element that allows users to show and hide sections of related content on a page."
     },
     {
         id: '4',
         question: "What are accordion components?",
-        answer: "Accordion components are user interface elements"
+        answer: "The accordion component is a UI element that allows users to show and hide sections of related content on a page."
     },
 ];
 
@@ -29,9 +29,32 @@ function createAccordionData(){
         `<div class= "accordion_item">
             <div class= "accordion_title">
                 <h3>${dataitem.question}</h3>
-                <i class= "fa-solid fa-arrow-up"></i>
+                <i class= "fa-solid fa-arrow-down"></i>
             </div>
-        </div>`)
+            <div class= "accordion-content">
+                <p>${dataitem.answer}</p>
+            </div>
+        </div>`
+    ).join("");
+
 }
 
 createAccordionData();
+
+const getAccordionTitle = document.querySelectorAll(".accordion_title")
+
+getAccordionTitle.forEach(currentItem=> {
+    currentItem.addEventListener('click', (event)=> {
+            if(currentItem.classList.contains("active")){
+                currentItem.classList.remove("active")
+            }
+            else{
+                let getAlredyAddedActiveClasses = document.querySelectorAll('.active');
+                getAlredyAddedActiveClasses.forEach(currentActiveItem=> {
+                    currentActiveItem.classList.remove("active");
+                });
+
+                currentItem.classList.add("active");
+            }
+    })
+})
