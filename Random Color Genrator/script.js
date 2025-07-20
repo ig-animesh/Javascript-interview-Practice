9db4156f6db497fd8320b02cc09daaaac6620865
Animesh <animesh.h2004@gmail.com>
2025-07-21 00:04:38 +0530

    build more on random color gen

---
+++ Random Color Genrator/script.js
@@ -0,0 +1,15 @@
+const hexBtn = document.querySelector('.hex-btn');
+const hexColorValue = document.querySelector('.hex-color-value');
+const hexColorContainer = document.querySelector('.hex-color-container')
+
+hexBtn.addEventListener("click", ()=> {
+        let characterSet = '0123456789ABCDEF';
+        let hexColorOutput = "";
+
+        for( let i = 0, charSetLength = characterSet.length; i<6; ++i)
+        {
+            hexColorOutput += characterSet.charAt(Math.floor(Math.random()* charSetLength));
+        }   
+      hexColorValue.textContent = `#${hexColorOutput}`;
+      hexColorContainer.style.backgroundColor = `#${hexColorOutput}`;
+});
