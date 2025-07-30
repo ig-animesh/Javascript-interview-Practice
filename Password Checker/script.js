13fde1805a16eb8335b19cb669f6b44888385395
Animesh <animesh.h2004@gmail.com>
2025-07-31 01:01:14 +0530

    Password checker with all checks done

--- Password Checker/script.js
+++ Password Checker/script.js
@@ -6,12 +6,34 @@ passwrdInput.addEventListener('input', function(){
     console.log(passwrdInput.value)
     let password = passwrdInput.value;
     if(password.length <8) {
-        passwrdOutput.innerText = 'Password is too short'
+        passwrdOutput.innerText = 'Password is weak'
         passwrdOutput.style.color = 'red'
     }
     else{
-        passwrdOutput.innerText = 'Password is too long'
-        passwrdOutput.style.color = 'green'
+        // passwrdOutput.innerText = 'Password is too long'
+        // passwrdOutput.style.color = 'green'
+
+        if(password.search == /[a-z]/){
+            passwrdOutput.innerText = 'Password is missing a lowercase letter'
+            passwrdOutput.style.color = 'red'
+        }
+        else if (password.search == /[A-Z]/){
+            passwrdOutput.innerText = 'Password is missing a uppercase letter'
+            passwrdOutput.style.color = 'red'
+        }
+        else if (password.search == /[0-9]/){
+            passwrdOutput.innerText = 'Password is missing a numeric letter'
+            passwrdOutput.style.color = 'red'
+        }
+        else if (password.search == /[!\@\#\$\%\^\&\*\(\)\{\}\<\>?\-]/){
+            passwrdOutput.innerText = 'Password is missing a special character'
+            passwrdOutput.style.color = 'red'
+        }
+        else{
+            passwrdOutput.innerText = 'Password is strong'
+            passwrdOutput.style.color = 'lightgreen'
+            passwrdOutput.style.fontSize = '20px'
+        }
 
     }
 });
